1ca2523765ff7ac73430f2516a48c72d992e9068
Hamid <hamidmsukardi@gmail.com>
2025-04-16 19:33:46 +0700

    wishes reveal text

--- src/components/Poem.jsx
+++ src/components/Poem.jsx
@@ -1,6 +1,8 @@
+import styles from '../styles/Poem.module.css';
+
 const Poem = () => {
     return (
-      <section className="bg-pink-100 py-10 px-4 text-center">
+      <section className={styles.poem}>
         <h2 className="text-3xl font-semibold mb-6">Puisi</h2>
         <p className="italic max-w-2xl mx-auto">
           Di hari bahagia ini<br />
--- src/components/Wishes.jsx
+++ src/components/Wishes.jsx
@@ -1,16 +1,47 @@
-import styles from '../styles/Wishes.module.css';
+'use client'
 
-export default function Wishes() {
-    return (
-      <section className={styles.wishes}>
-        <h2 className="text-3xl font-semibold text-center mb-6">Ucapan</h2>
-        <ul className="space-y-4 max-w-xl mx-auto">
-          <li>🎈 Semoga panjang umur dan sehat selalu!</li>
-          <li>💖 Semoga segala harapanmu tercapai!</li>
-          <li>🥳 Nikmati hari spesialmu!</li>
-        </ul>
-      </section>
-    );
-  };
+import styles from '../styles/Wishes.module.css';
+import { useScroll, motion, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+
+const Wishes = () => {
+  const value = `Hari ini adalah momen yang sangat istimewa karena dunia diberkahi dengan kehadiranmu yang luar biasa.
+    Hari ini adalah momen yang sangat istimewa karena dunia diberkahi dengan kehadiranmu yang luar biasa.
+    Hari ini adalah momen yang sangat istimewa karena dunia diberkahi dengan kehadiranmu yang luar biasa.`;
+
+  const element = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ['start 0.6', 'start 0.2']
+  })
+
+  const words = value.split(" ")
+
+  return (
+    <p 
+      className={styles.wishes}
+      ref={element}
+    >
+      {
+        words.map( (word, i) => {
+            const start = i / words.length;
+            const end = start + (1 / words.length);
+            return <Word key={i} range={[start, end]} progress={scrollYProgress}>{word}</Word>
+        })
+      }
+    </p>
+  );
+};
   
-  
+const Word = ({children, range, progress}) => {
+  const opacity = useTransform(progress, range, [0, 1])
+
+  return (
+      <span className={styles.word}>
+          <span className={styles.shadow}>{children}</span>
+          <motion.span style={{ opacity }}>{children}</motion.span>
+      </span>
+  )
+}
+
+export default Wishes
